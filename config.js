10734fad328f44d5a76dbd6b7f20ac21e1d03994
Flexortech <151713663+Flexortech@users.noreply.github.com>
2023-11-22 11:56:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['0724660864', 'flexortech', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
